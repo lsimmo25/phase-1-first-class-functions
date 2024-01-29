a357c354326792bf3a7c5c8cd823533db6e68d98
Lyle Simmons <lylesimmons125@gmail.com>
2024-01-29 18:51:25 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        console.log('Hello');
+    };
+}
+
+function returnsAnAnonymousFunction(){
+    return function () {
+        console.log("bye");
+    }
+}
